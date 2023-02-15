cd9d5d04f8fe4581b1aa85e13b8be6f7b30e5cb7
Diana Barbar <barbar.di@yandex.ru>
2023-02-15 12:25:18 +0300

    исправила оибки в коде, финальная версия

--- script.js/index.js
+++ script.js/index.js
@@ -1,49 +1,65 @@
-// отерытие и закрытие попапа
 let profileEdit = document.querySelector('.profile-info__edit-button');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.button-close');
+let userNameElement = document.querySelector('.profile-info__title');
+let userDescriptionElement = document.querySelector('.profile-info__subtitle');
+let formElement = document.querySelector('.popup__form');
+let inputName = document.querySelector('.popup-input__name'); 
+let inputDescription = document.querySelector('.popup-input__description'); 
+let buttonSubmit = document.querySelector('.button-submit');
 
 function showPopup () {
   popup.classList.add('popup_opened');
+  inputName.value = userNameElement.textContent;
+  inputDescription.value = userDescriptionElement.textContent;
 }
- 
-profileEdit.addEventListener('click', showPopup); 
 
 function closePopup () {
   popup.classList.remove('popup_opened');
 }
 
-popupClose.addEventListener('click', closePopup);
-
-// function showAndClosePopup () {
- // popup.classList.toggle('popup_opened');
-//}
-
-//profileEdit.addEventListener('click', showAndClosePopup);
-
-//popupClose.addEventListener('click', showAndClosePopup);
-
-
-//добавление и сохранение информации
-
-let userName = document.querySelector('.profile-info__title');
-let userDescription = document.querySelector('.profile-info__subtitle');
-
-let inputPopupName = document.querySelector('.popup-input__name');
-let inputPopupDescription = document.querySelector('popup-input__description');
-
-inputPopupName.value = userName.textContent;
-inputPopupDescription.value = userDescription.textContent;
-
-let popupSubmit = document.querySelector('.button-submit');
-popupSubmit.addEventListener('click', save);
-
-function save () {
-  userName.textContent = inputPopupName.value;
-  userDescription.textContent = inputPopupDescription.value;
-  popup.classList.add('popup__opened');
-  popup.classList.remove('popup__opened');
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    userNameElement.textContent = inputName.value; 
+    userDescriptionElement.textContent = inputDescription.value;
+    closePopup();
 }
+
+profileEdit.addEventListener('click', showPopup); 
+popupClose.addEventListener('click', closePopup);
+buttonSubmit.addEventListener('click', handleFormSubmit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

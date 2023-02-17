fb9170fa66cc082f931ca1707ccd75d72f491fe9
Diana Barbar <barbar.di@yandex.ru>
2023-02-17 12:51:03 +0300

    исправила 1 ревью замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 let profileEdit = document.querySelector('.profile-info__edit-button');
 let popup = document.querySelector('.popup');
-let popupClose = document.querySelector('.button-close');
+let popupClose = document.querySelector('.popup__button-close');
 let userNameElement = document.querySelector('.profile-info__title');
 let userDescriptionElement = document.querySelector('.profile-info__subtitle');
 let formElement = document.querySelector('.popup__form');
-let inputName = document.querySelector('.popup__input_name'); 
-let inputDescription = document.querySelector('.popup__input_description'); 
-let buttonSubmit = document.querySelector('.button-submit');
+let inputName = document.querySelector('.popup__input_type_name'); 
+let inputDescription = document.querySelector('.popup__input_type_description'); 
+let buttonSubmit = document.querySelector('.popup__button-submit');
 
 function showPopup () {
   popup.classList.add('popup_opened');
@@ -27,39 +27,4 @@ function handleFormSubmit (evt) {
 
 profileEdit.addEventListener('click', showPopup); 
 popupClose.addEventListener('click', closePopup);
-buttonSubmit.addEventListener('click', handleFormSubmit);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+formElement.addEventListener('submit', handleFormSubmit);

2cf90de7176c9ac00654e1b3e79917cbf34beefb
Diana Barbar <barbar.di@yandex.ru>
2023-02-25 22:45:41 +0300

    открытие/закрытие нового попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,74 @@
-let profileEdit = document.querySelector('.profile-info__edit-button');
-let popup = document.querySelector('.popup');
-let popupClose = document.querySelector('.popup__button-close');
-let userNameElement = document.querySelector('.profile-info__title');
-let userDescriptionElement = document.querySelector('.profile-info__subtitle');
-let formElement = document.querySelector('.popup__form');
-let inputName = document.querySelector('.popup__input_type_name'); 
-let inputDescription = document.querySelector('.popup__input_type_description'); 
+ let profileEdit = document.querySelector('.profile-info__edit-button');
+ let popup = document.querySelector('.popup');
+ let popupClose = document.querySelector('.popup__button-close');
+ let userNameElement = document.querySelector('.profile-info__title');
+ let userDescriptionElement = document.querySelector('.profile-info__subtitle');
+ let formElement = document.querySelector('.popup__form');
+ let inputName = document.querySelector('.popup__input_type_name'); 
+ let inputDescription = document.querySelector('.popup__input_type_description'); 
 
-function showPopup () {
-  popup.classList.add('popup_opened');
-  inputName.value = userNameElement.textContent;
-  inputDescription.value = userDescriptionElement.textContent;
-}
+ function showPopup () {
+   popup.classList.add('popup_opened');
+   inputName.value = userNameElement.textContent;
+   inputDescription.value = userDescriptionElement.textContent;
+ }
 
-function closePopup () {
-  popup.classList.remove('popup_opened');
-}
+ function closePopup () {
+   popup.classList.remove('popup_opened');
+ }
 
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-    userNameElement.textContent = inputName.value; 
-    userDescriptionElement.textContent = inputDescription.value;
-    closePopup();
-}
+ function handleFormSubmit (evt) {
+     evt.preventDefault(); 
+     userNameElement.textContent = inputName.value; 
+     userDescriptionElement.textContent = inputDescription.value;
+     closePopup();
+ }
 
 profileEdit.addEventListener('click', showPopup); 
 popupClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
+
+// начало 5 проектной работы
+let buttonAdd = document.querySelector('.profile__add-button');
+let popupAdd = document.querySelector('.popup_add');
+let buttonCloseCard = document.querySelector('.popup__button-close_card');
+
+function showPopupAdd () {
+  popupAdd.classList.add('popup_opened');
+}
+
+function closePopupAdd () {
+  popupAdd.classList.remove('popup_opened');
+}
+
+buttonAdd.addEventListener('click', showPopupAdd);
+buttonCloseCard.addEventListener('click', closePopupAdd);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
